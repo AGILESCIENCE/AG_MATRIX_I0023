b4cbfa29aba9f07844246b5f98ba0764b355b49a
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2014-08-18 17:30:18 +0200

    I0023

---
+++ install.sh
@@ -0,0 +1,12 @@
+cd AG_MATRIX_I0023
+make install prefix=$AGILE/model
+cd ..
+cd AG_MATRIX_GC_I0023
+make install prefix=$AGILE/model
+cd ..
+cd AG_MATRIX_I0023_CONV
+make install prefix=$AGILE/model
+cd ..
+cd AG_MATRIX_I0023_DISPCONV
+make install prefix=$AGILE/model
+cd ..
